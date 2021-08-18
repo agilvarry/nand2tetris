dfde3c3e1efbd46d853b3301f316638cc5cf0929
Alex Gilvarry <agilvarry@utah.gov>
2021-08-18 15:33:05 -0600

    prep for square

--- 10/compare.bat
+++ 10/compare.bat
@@ -1,17 +1,17 @@
 @echo off
 cd C:\Users\agilvarry\Documents\nand2tetris\tools
+@REM echo on
+@REM call TextComparer C:\Users\agilvarry\Documents\github\nand2tetris\10\ExpressionLessSquare\Main.xml C:\Users\agilvarry\Documents\github\nand2tetris\10\ExpressionLessSquare\output\Main.xml
 echo on
-call TextComparer C:\Users\agilvarry\Documents\github\nand2tetris\10\ExpressionLessSquare\MainT.xml C:\Users\agilvarry\Documents\github\nand2tetris\10\ExpressionLessSquare\output\MainT.xml
-echo on
-call TextComparer C:\Users\agilvarry\Documents\github\nand2tetris\10\ExpressionLessSquare\SquareT.xml C:\Users\agilvarry\Documents\github\nand2tetris\10\ExpressionLessSquare\output\SquareT.xml
-echo on
-call TextComparer C:\Users\agilvarry\Documents\github\nand2tetris\10\ExpressionLessSquare\SquareGameT.xml C:\Users\agilvarry\Documents\github\nand2tetris\10\ExpressionLessSquare\output\SquareGameT.xml
-echo on
-call TextComparer C:\Users\agilvarry\Documents\github\nand2tetris\10\ArrayTest\MainT.xml C:\Users\agilvarry\Documents\github\nand2tetris\10\ArrayTest\output\MainT.xml
-echo on
-call TextComparer C:\Users\agilvarry\Documents\github\nand2tetris\10\Square\MainT.xml C:\Users\agilvarry\Documents\github\nand2tetris\10\Square\output\MainT.xml
-echo on
-call TextComparer C:\Users\agilvarry\Documents\github\nand2tetris\10\Square\SquareT.xml C:\Users\agilvarry\Documents\github\nand2tetris\10\Square\output\SquareT.xml
-echo on
-call TextComparer C:\Users\agilvarry\Documents\github\nand2tetris\10\Square\SquareGameT.xml C:\Users\agilvarry\Documents\github\nand2tetris\10\Square\output\SquareGameT.xml
-pause
+call TextComparer C:\Users\agilvarry\Documents\github\nand2tetris\10\ExpressionLessSquare\Square.xml C:\Users\agilvarry\Documents\github\nand2tetris\10\ExpressionLessSquare\output\Square.xml
+@REM echo on
+@REM call TextComparer C:\Users\agilvarry\Documents\github\nand2tetris\10\ExpressionLessSquare\SquareGame.xml C:\Users\agilvarry\Documents\github\nand2tetris\10\ExpressionLessSquare\output\SquareGame.xml
+@REM echo on
+@REM call TextComparer C:\Users\agilvarry\Documents\github\nand2tetris\10\ArrayTest\Main.xml C:\Users\agilvarry\Documents\github\nand2tetris\10\ArrayTest\output\Main.xml
+@REM echo on
+@REM call TextComparer C:\Users\agilvarry\Documents\github\nand2tetris\10\Square\Main.xml C:\Users\agilvarry\Documents\github\nand2tetris\10\Square\output\Main.xml
+@REM echo on
+@REM call TextComparer C:\Users\agilvarry\Documents\github\nand2tetris\10\Square\Square.xml C:\Users\agilvarry\Documents\github\nand2tetris\10\Square\output\Square.xml
+@REM echo on
+@REM call TextComparer C:\Users\agilvarry\Documents\github\nand2tetris\10\Square\SquareGame.xml C:\Users\agilvarry\Documents\github\nand2tetris\10\Square\output\SquareGame.xml
+@REM pause
