ce5492be970efd9e393d41cfadde7d9e1d3ca1b3
Alex Gilvarry <agilvarry@utah.gov>
2021-09-02 09:58:36 -0600

    breaking point

--- 11/VMWriter.py
+++ 11/VMWriter.py
@@ -5,16 +5,17 @@ def write_pop(segment, index):
     return  f"pop {segment} {index}\n"
 
 def write_arithmetic(command):
-    return
+    print(command)
+    return f"{command}\n"
 
 def write_label(label):
-    return
+    return f"label {label}\n"
 
 def write_goto(label):
-    return
+    return f"goto {label}\n"
 
 def write_if(label):
-    return
+    return f"if-goto {label}\n"
 
 def write_call(name, n_args):
     return f"call {name} {n_args}\n"
@@ -23,4 +24,4 @@ def write_function(name, n_vars):
     return f"function {name} {n_vars}\n"
 
 def write_return():
-    return "push constant 0\nreturn"
+    return "push constant 0\nreturn\n"
